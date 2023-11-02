0efb7df6871b761671685eb0ee9923be750f51fe
lby132 <quddbscjswow@naver.com>
2023-11-02 20:40:17 +0900

    8

---
+++ src/main/java/main/FunctionalInterfaceExamples.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FunctionalInterfaceExamples {
+    public static void main(String[] args) {
+        final Function<String, Integer> toInt = value -> Integer.parseInt(value);
+
+        final Integer number = toInt.apply("100");
+        System.out.println(number);
+
+        final Function<Integer, Integer> identity = Function.identity();
+
+        System.out.println(identity.apply(100));
+
+        final Consumer<String> print = value -> System.out.println(value);
+
+        print.accept("Hello");
+
+        Predicate<Integer> isPositive = i -> i > 0;
+
+        System.out.println(isPositive.test(1));
+        System.out.println(isPositive.test(0));
+        System.out.println(isPositive.test(-1));
+
+        final List<Integer> numbers = Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5);
+
+        List<Integer> positiveNumbers = new ArrayList<>();
+        for (Integer num : numbers) {
+            if (isPositive.test(num)) {
+                positiveNumbers.add(num);
+            }
+        }
+        System.out.println("positive integers: " + positiveNumbers);
+        System.out.println("positive integers2: " + filter(numbers, isPositive));
+        System.out.println("positive integers3: " + filter(numbers, i -> i > 0));
+    }
+
+    private static <T> List filter(List<T> list, Predicate<T> filter) {
+        List<T> result = new ArrayList<>();
+        for (T input : list) {
+            if (filter.test(input)) {
+                result.add(input);
+            }
+        }
+        return result;
+    }
+}
