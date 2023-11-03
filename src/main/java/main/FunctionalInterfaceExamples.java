de3343657b725e7fce7bfafd4befbe1c75db9105
lby132 <quddbscjswow@naver.com>
2023-11-03 16:20:59 +0900

    8

--- src/main/java/main/FunctionalInterfaceExamples.java
+++ src/main/java/main/FunctionalInterfaceExamples.java
@@ -3,12 +3,14 @@ package main;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class FunctionalInterfaceExamples {
-    public static void main(String[] args) {
+    public void run4FunctionalInterfaces() {
         final Function<String, Integer> toInt = value -> Integer.parseInt(value);
 
         final Integer number = toInt.apply("100");
@@ -39,6 +41,34 @@ public class FunctionalInterfaceExamples {
         System.out.println("positive integers: " + positiveNumbers);
         System.out.println("positive integers2: " + filter(numbers, isPositive));
         System.out.println("positive integers3: " + filter(numbers, i -> i > 0));
+
+        final Supplier<String> helloSupplier = () -> "Hello ";
+        System.out.println(helloSupplier.get() + "world");
+
+        long start = System.currentTimeMillis();
+        printIfValidIndex(0, () -> getVeryExpensiveValue());
+        printIfValidIndex(-1, () -> getVeryExpensiveValue());
+        printIfValidIndex(-2, () -> getVeryExpensiveValue());
+        System.out.println("It took " + ((System.currentTimeMillis() - start) / 1000) + " seconds.");
+    }
+
+    private static String getVeryExpensiveValue() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Kevin";
+    }
+
+//    private static void printIfValidIndex(int number, String value) {
+    private static void printIfValidIndex(int number, Supplier<String> valueSupplier) {
+        if (number >= 0) {
+            System.out.println("The value is " + valueSupplier.get() + ".");
+        } else {
+            System.out.println("Invalid");
+        }
     }
 
     private static <T> List filter(List<T> list, Predicate<T> filter) {
@@ -50,4 +80,16 @@ public class FunctionalInterfaceExamples {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        println("Area is ", 12, 20, (message, length, width) -> (message + (length * width)));
+    }
+
+    private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function) {
+        System.out.println(function.apply(t1, t2, t3));
+    }
+}
+
+interface Function3<T1, T2, T3, R> {
+    R apply(T1 t1, T2 t2, T3 t3);
 }
