243817bed77fee337c99d7da546575da2570621c
lby132 <quddbscjswow@naver.com>
2023-11-09 17:25:33 +0900

    8

---
+++ src/main/java/main/StreamPrelude.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class StreamPrelude {
+
+    public static void main(String[] args) {
+        final int abs1 = Math.abs(-1);
+        final int abs2 = Math.abs(1);
+
+        System.out.println("abs1 == abs2 is " + (abs1 == abs2));
+
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(
+                map(numbers, i -> i * 2)
+        );
+        System.out.println(
+                map(numbers, i -> i)
+        );
+        //identity function 을 사용하여 null신경쓰지 않아도 됨
+        System.out.println(
+                map(numbers, Function.identity())
+        );
+    }
+
+    private static <T, R> List<R> map(final List<T> list, final Function<T, R> mapper) {
+        final List<R> result = new ArrayList<>();
+        for (T t : list) {
+            result.add(mapper.apply(t));
+        }
+
+        return result;
+    }
+}
