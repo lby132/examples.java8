3aa8e4f9a05a31fb3ecdbd692ac55562e812ebe9
lby132 <quddbscjswow@naver.com>
2023-11-14 15:54:33 +0900

    8

--- src/main/java/main/StreamExamples3.java
+++ src/main/java/main/StreamExamples3.java
@@ -36,9 +36,11 @@ public class StreamExamples3 {
                 IntStream.of(1, 2, 3, 4, 5)
                         .sum());
 
+        System.out.println("\n======================================");
+        System.out.println("Total Price: " +
         products.stream()
                 .map(product -> product.getPrice())
-                .reduce(BigDecimal.ZERO, (product1, product2) -> product1.add(product2));
+                .reduce(BigDecimal.ZERO, (product1, product2) -> product1.add(product2)));
 
         System.out.println("\n======================================");
         System.out.println("Total Price of Products.price >= 30: " +
@@ -46,5 +48,46 @@ public class StreamExamples3 {
                         .filter(product -> product.getPrice().compareTo(new BigDecimal("30")) >= 0)
                         .map(product -> product.getPrice())
                         .reduce(BigDecimal.ZERO, (price1, price2) -> price1.add(price2)));
+
+        final OrderedItem item1 = new OrderedItem(1L, products.get(0), 1);
+        final OrderedItem item2 = new OrderedItem(2L, products.get(2), 3);
+        final OrderedItem item3 = new OrderedItem(3L, products.get(4), 10);
+
+        final Order order = new Order(1L, Arrays.asList(item1, item2, item3));
+
+        System.out.println("\n======================================");
+        System.out.println("order.totalPrice(): " + order.totalPrice());
+    }
+
+    static class OrderedItem {
+        private Long id;
+        private Product product;
+        private int quantity;
+
+        public BigDecimal getTotalPrice() {
+            return product.getPrice().multiply(new BigDecimal(quantity));
+        }
+
+        public OrderedItem(Long id, Product product, int quantity) {
+            this.id = id;
+            this.product = product;
+            this.quantity = quantity;
+        }
+    }
+
+    static class Order {
+        private Long id;
+        private List<OrderedItem> items;
+
+        public Order(Long id, List<OrderedItem> items) {
+            this.id = id;
+            this.items = items;
+        }
+
+        public BigDecimal totalPrice() {
+            return items.stream()
+                    .map(item -> item.getTotalPrice())
+                    .reduce(BigDecimal.ZERO, (price1, price2) -> price1.add(price2));
+        }
     }
 }
