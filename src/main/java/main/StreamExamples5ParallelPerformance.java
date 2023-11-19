7d09244fa9c094a6bfb617a5c4a4d9110f42b604
lby132 <quddbscjswow@naver.com>
2023-11-19 13:57:26 +0900

    8

--- src/main/java/main/StreamExamples5ParallelPerformance.java
+++ src/main/java/main/StreamExamples5ParallelPerformance.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class StreamExamples5ParallelPerformance {
@@ -13,5 +14,36 @@ public class StreamExamples5ParallelPerformance {
 
     public static long sequentialSum(long n) {
         return Stream.iterate(1L, i -> i + 1).limit(n).reduce(Long::sum).get();
+    }
+
+    public static long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1).limit(n).parallel().reduce(Long::sum).get();
+    }
+
+    public static long rangedSum(long n) {
+        return LongStream.rangeClosed(1, n).reduce(Long::sum).getAsLong();
+    }
+
+    public static long parallelRangedSum(long n) {
+        return LongStream.rangeClosed(1, n).parallel().reduce(Long::sum).getAsLong();
+    }
+
+    public static void main(String[] args) {
+        final long n = 10_000_000;
+        final long start1 = System.currentTimeMillis();
+        System.out.println(" iterativeSum(n): " + iterativeSum(n));
+        System.out.println("                  " + (System.currentTimeMillis() - start1) + " ms \n");
+        final long start2 = System.currentTimeMillis();
+        System.out.println(" iterativeSum(n): " + sequentialSum(n));
+        System.out.println("                  " + (System.currentTimeMillis() - start2) + " ms \n");
+        final long start3 = System.currentTimeMillis();
+        System.out.println(" iterativeSum(n): " + parallelSum(n));
+        System.out.println("                  " + (System.currentTimeMillis() - start3) + " ms \n");
+        final long start4 = System.currentTimeMillis();
+        System.out.println(" iterativeSum(n): " + rangedSum(n));
+        System.out.println("                  " + (System.currentTimeMillis() - start4) + " ms \n");
+        final long start5 = System.currentTimeMillis();
+        System.out.println(" iterativeSum(n): " + parallelRangedSum(n));
+        System.out.println("                  " + (System.currentTimeMillis() - start5) + " ms \n");
     }
 }
